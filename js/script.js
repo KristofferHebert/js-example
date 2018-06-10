68fc3b15a7e739979018f1054ce2f83ce0de6098
kristofferhebert <kristofferhebert>
2018-06-09 20:18:55 -0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+(function(){
+
+    // Query for iframe and link items
+    function bootstrap(){
+        var content = document.getElementById("content");
+        var menuLinks = document.querySelectorAll("li a");
+        
+        // Update iframe depending on menu link clicked
+        function changePage(li){
+            return function(e){
+                e.preventDefault();
+                var page = li.href.split("#")[1];
+                content.src = "/" + page;
+            }
+        }
+
+        // Bind function to each link item
+        for(var i = 0; i < menuLinks.length; i++ ){
+            menuLinks[i].addEventListener("click", changePage(menuLinks[i]), true);
+        }    
+    }
+    
+    // Fire on page load
+    window.onload = bootstrap;
+
+})();
